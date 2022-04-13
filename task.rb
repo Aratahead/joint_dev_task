8ed9b45ddf80692984fa3671682e63e25fff899f
Aratahead <maramhisa.1229@gmail.com>
2022-04-13 20:08:25 +0900

    20解答

--- task.rb
+++ task.rb
@@ -182,9 +182,9 @@ class UserQ18
 
   def introduce
     if @age >=20
-      return "こんにちは,#{@name}と申します。宜しくお願いいたします。"
+      "こんにちは,#{@name}と申します。宜しくお願いいたします。"
     else
-      return "はいさいまいど~,#{@name}です!!!"
+      "はいさいまいど~,#{@name}です!!!"
     end
   end
 
@@ -215,10 +215,31 @@ end
 
 class UserQ20
   # 以下に回答を記載
+  attr_accessor :name,:age
+  def initialize(name:,age:)
+    @name=name
+    @age=age
+  end
 end
 
 class Zoo
   # 以下に回答を記載
+  def initialize(name:,entry_fee:)
+    @entry_fee=entry_fee
+  end
+
+  def info_entry_fee(user)
+    if user.age<4
+      @fee=@entry_fee[:infant]
+    elsif user.age<16
+      @fee=@entry_fee[:children]
+    elsif user.age<66
+      @fee=@entry_fee[:adult]
+    else
+      @fee=@entry_fee[:senior]
+    end
+    puts "#{user.name}さんの入場料金は#{@fee}円です。"
+  end
 end
 
 
